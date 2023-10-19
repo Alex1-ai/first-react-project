b73e17b45e4ee88146b29350013693db3d99ae33
Alex1.ai <alexanderemmanuel1719@gmail.com>
2023-10-19 17:08:23 +0000

    my first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  
+  return (
+    <div className="App">
+      
+      <div>
+        <h3>Blog Title 1</h3>
+        <p>Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
